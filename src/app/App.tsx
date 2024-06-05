f1a33d9a045054ebe58cca42d0a15ffdcd2a7f9d
@konyushkov.nk <konyushkov.nk@esphere.ru>
2024-06-05 11:08:14 +0300

    Добавлена информация о себе

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -8,8 +8,18 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Текст писать тут
+          Привет!
         </p>
+        <p>
+          Меня зовут Николай, я разработчик интерфейсов в компании СберКорус.
+        </p>
+        <p>
+          Пишу на TypeScript, пользуюсь React.
+        </p>
+        <p>
+          Знаком с JS, поверхностным Angular. Есть опыт настройки webpack и работы с sass.
+        </p>
+        <p>Хочу углубить познания в ReactJS для написания более осмысленного и функционального кода</p>
       </header>
     </div>
   );
